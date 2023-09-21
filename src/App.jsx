a6858a4956de1811bdfc5e8ea6d8f3a1fb8970d1
Robert Blake <rblake14@gmail.com>
2023-09-21 15:47:55 -0400

    Add font, add social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
 import Experience from "./components/Experience";
@@ -6,6 +6,7 @@ import * as THREE from "three";
 import colors from "./lib/colors";
 import { useState } from "react";
 import { FaGithub, FaLinkedin, FaBlog } from "react-icons/fa";
+import WebFont from "webfontloader";
 
 const stationHeader = ["ABOUT ME", "MY WORK", "CONTACT"];
 const stationText = [
@@ -16,27 +17,63 @@ const stationText = [
 
 function App() {
   const [station, setStation] = useState(1);
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Space Mono"],
+      },
+    });
+  }, []);
 
   return (
     <>
-      <div style={{ width: "100vw", display: "grid", placeContent: "center", position: "absolute", zIndex: "1", color: colors.blue }}>
+      <div
+        style={{
+          width: "100vw",
+          display: "grid",
+          placeContent: "center",
+          position: "absolute",
+          zIndex: "1",
+          color: colors.blue,
+          fontFamily: "Space Mono",
+        }}
+      >
         <div style={{ width: "40vw" }}>
           <h1 style={{ fontSize: "4rem", fontStyle: "bold", textAlign: "center" }}>{stationHeader[station]}</h1>
           <p style={{ width: "100%" }}>{stationText[station]}</p>
         </div>
       </div>
 
-      <div style={{ position: "absolute", zIndex: "1", color: "white", display: "flex", flexDirection: "column", alignContent: "center" }}>
+      <div
+        style={{
+          position: "absolute",
+          zIndex: "1",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+          fontFamily: "Space Mono",
+          paddingLeft: ".5em",
+          lineHeight: ".5em",
+          gap: ".1em",
+        }}
+      >
         <h2>robert blake</h2>
         <ul style={{ display: "flex", gap: "1em", justifyContent: "space-evenly", margin: "0", padding: "0" }}>
           <li style={{ display: "inline" }}>
-            <FaGithub />
+            <a style={{ color: "white" }} href={"https://github.com/radiorobert"}>
+              <FaGithub />
+            </a>
           </li>
           <li style={{ display: "inline" }}>
-            <FaLinkedin />
+            <a style={{ color: "white" }} href={"https://www.linkedin.com/in/rblake14/"}>
+              <FaLinkedin />
+            </a>
           </li>
           <li style={{ display: "inline" }}>
-            <FaBlog />
+            <a style={{ color: "white" }} href={"https://radio.bearblog.dev"}>
+              <FaBlog />
+            </a>
           </li>
         </ul>
       </div>
