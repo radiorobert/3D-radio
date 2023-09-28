b2e30a8559773c460fa6546673e2688c09305d82
Robert Blake <rblake14@gmail.com>
2023-09-28 11:49:43 -0400

    Finish up personal page

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import s from "./personal.module.css";
 
 function Personal() {
   return (
-    <>
-      <p>radio.bearblog.dev, koolaid, sc2-helper</p>
-    </>
+    <div className={s.main}>
+      <p>
+        <a className={s.linkColor} href={"https://radio.bearblog.dev"}>
+          radio.bearblog.dev
+        </a>
+        : my blog, where you can find updates on things I'm working on.
+      </p>
+      <p>
+        <a className={s.linkColor} href={"https://radio.bearblog.dev/koolaid-man-wallbreak/"}>
+          koolaid crash
+        </a>
+        : a game using react-three-fiber
+      </p>
+      <p>
+        <a className={s.linkColor} href={"https://radio.bearblog.dev/radio-showcase-page/"}>
+          this site (blog link)
+        </a>
+        : built using Three.js to showcase my work
+      </p>
+    </div>
   );
 }
 
